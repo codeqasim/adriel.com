613fdd009500495ec617e86f06ec52a9170830d6
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2020-05-07 06:17:39 -0700

    i

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
-<?php $title = "Homepage"; ?> 
+<?php $title = "Homepage"; ?>
 <?php include "header.php"; ?>
 
-
-<p>Body</p>
-
+<div class="container">
+<p style="margin:400px 0">Body</p>
+</div>
 
 <?php include "footer.php"; ?>
 
